1f8af1bbe5fedc9533098868dc5afbec5a49ed3f
David De Anda <omarandstuff@gmail.com>
2023-03-26 01:13:00 -0600

    Refine connect provider to recognize unknown provider

--- src/defaults/main/ConnectProvider.universal-auth-dynamic.ts
+++ src/defaults/main/ConnectProvider.universal-auth-dynamic.ts
@@ -11,16 +11,20 @@ export default class ConnectProviderDynamic {
       const dynamicName = `get-${provider}-user-data`
       const providerKeys = authentication.options.providerKeys[provider]
 
-      const providerDataResult: ProviderDataResult = await authentication.performDynamic(dynamicName, { token, keys: providerKeys })
+      if (authentication.dynamics[dynamicName]) {
+        const providerDataResult: ProviderDataResult = await authentication.performDynamic(dynamicName, { token, keys: providerKeys })
 
-      if (!providerDataResult.error) {
-        const { attributes } = providerDataResult
-        authentication.performDynamicSync('set-authenticatable-provider-id', { authenticatable, provider, id: attributes.id })
-        await authentication.performDynamic('save-authenticatable', { authenticatable })
+        if (!providerDataResult.error) {
+          const { attributes } = providerDataResult
+          authentication.performDynamicSync('set-authenticatable-provider-id', { authenticatable, provider, id: attributes.id })
+          await authentication.performDynamic('save-authenticatable', { authenticatable })
 
-        return { status: 'success', authenticatable }
+          return { status: 'success', authenticatable }
+        } else {
+          return { status: 'failure', message: 'provider-error' }
+        }
       } else {
-        return { status: 'failure', message: 'provider-error' }
+        return { status: 'failure', message: 'unknown-provider' }
       }
     } else {
       return { status: 'warning', message: 'already-connected' }
