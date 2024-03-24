5755747f1b7c51381216deec8060d747efbd561d
David De Anda <omarandstuff@gmail.com>
2024-03-24 11:49:16 -0600

    Default 10 retries in CI

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,7 +2,7 @@ import TestAuthenticatable from './__fixtures__/TestAuthenticatable'
 
 // node > 19 has some issues with fetch closing sockets on consecutive requests
 if (process.env.CI || process.versions.node.startsWith('20')) {
-  jest.retryTimes(5)
+  jest.retryTimes(10)
   jest.setTimeout(10000)
 }
 
